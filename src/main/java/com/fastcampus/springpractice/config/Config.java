a0c1b59f659409eb88822135e660d8e2e207317c
yb <dydqls5757@gmail.com>
2022-06-29 14:26:09 +0900

    complete #4 spring-configuration
    spring 구성 완료

--- src/main/java/com/fastcampus/springpractice/config/Config.java
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -2,9 +2,11 @@ package com.fastcampus.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan("com.fastcampus.springpractice")
 @Configuration
+@EnableWebMvc
 public class Config {
 
 }
